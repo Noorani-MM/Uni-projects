60c0b7e5da1229d7de7dda6c96cac1ea0fdffd4b
matio <matio.nora2000@gmail.com>
2022-12-17 15:32:45 +0330

    feat-project: practice 02 done

---
+++ Day 01/P02.py
@@ -0,0 +1,5 @@
+heigth = float(input("Enter your height (m): "))
+weight = float(input("Enter your weight (Kg): "))
+
+print("Your heigth is {0} m equal {1} sm".format(heigth, (heigth * 100)))
+print("Your weight is {0} Kg equal {1} g".format(weight, (weight * 1000)))
